ea2ed9b3a0fd2a255ba9d5f900d9199ec973b7d6
MarcManiez <marc.maniez@gmail.com>
2017-04-17 14:46:14 -0700

    Create base testing framework.

--- Marc_Maniez_Cafe_Shoppe/spec/baristaSpec.js
+++ Marc_Maniez_Cafe_Shoppe/spec/baristaSpec.js
@@ -1,3 +1,15 @@
+const fs = require('fs');
 const chai = require('chai');
 const expect = chai.expect;
 
+describe('cafe shoppe', () => {
+
+  const input = fs.readFileSync('./sample-data/input.json');
+  const output = fs.readFileSync('./sample-data/output_fifo.json');
+
+  describe('fifo', () => {
+    it('should process the orders on a first come, first served basis', () => {
+
+    });
+  });
+});

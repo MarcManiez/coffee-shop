5ccf62b8614a2bb0b14b4132ff7103dc2a43f016
MarcManiez <marc.maniez@gmail.com>
2017-04-17 23:31:31 -0700

    Create fifo queue methods.

--- Marc_Maniez_Cafe_Shoppe/src/fifo_queue.js
+++ Marc_Maniez_Cafe_Shoppe/src/fifo_queue.js
@@ -2,6 +2,16 @@ class Queue {
   constructor () {
     this.queue = [];
   }
+  enqueue(value) {
+    this.queue.push(value);
+    return value;
+  }
+  dequeue() {
+    return this.queue.shift();
+  }
+  isEmpty() {
+    return !this.queue.length;
+  }
 }
 
 module.exports = Queue;
